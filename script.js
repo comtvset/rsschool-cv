3aa65c66782f2210096db445922e259c3c33f994
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-07-10 21:08:43 +0200

    fix: style and add popup (Mon, Jul 10, 2023 09:08:43 PM)

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ function upFunction() {
 function downFunction() {
     document.documentElement.scrollTop += 10000;
 }
+
+
+
+const popup = document.querySelector('.popup');
+const popupLinks = document.querySelectorAll('.popup-link');
+
+function bodyLock() {
+    document.body.classList.toggle('lock');
+}
+
+function bodyUnlock() {
+    document.body.classList.remove('lock');
+}
+
+for (let i = 0; i < popupLinks.length; i++) {
+    popupLinks[i].addEventListener('click', (event) => {
+        popup.classList.toggle('open');
+        const Img = popupLinks[i].children[0].src;
+        popup.children[0].children[1].src = Img;
+        bodyLock();
+        document.getElementById("myBtnDown").style.display = "none";
+        document.getElementById("myBtnUp").style.display = "none";
+    });
+}
+
+function close() {
+    popup.addEventListener('click', (event) => {
+        if(event.target.classList.contains('popup-body')) {
+            popup.classList.toggle('open')
+            bodyUnlock();
+        }
+    })
+}
+close();
